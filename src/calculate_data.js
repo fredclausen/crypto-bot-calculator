d69386c5e783d20d86cec30ab07b5094d5ca5439
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-05-01 13:05:00 -0600

    basic functionality

--- src/calculate_data.js
+++ src/calculate_data.js
@@ -1,5 +1,35 @@
 export const calculate_data = {
-  total_cash: (free_cash = 0.0, cash_in_bots = 0.0) => {
-    return free_cash + cash_in_bots;
+  normalize_numbers: function (input_number = 0.0) {
+    return input_number !== undefined ? input_number.toFixed(2) : "0.00";
+  },
+
+  normalize_numbers_int: function (input_number = 0) {
+    return input_number ? input_number.toFixed(0) : "0";
+  },
+
+  total_cash: function (free_cash = 0.0, cash_in_bots = 0.0) {
+    return this.normalize_numbers(free_cash || 0 + cash_in_bots || 0);
+  },
+
+  amount_per_bot: function (
+    base_order_size,
+    safety_order_size,
+    safety_order_scaling,
+    max_safety_orders
+  ) {
+    // ensure all parameters are valid. Any undefined or 0 means the calculation cannot happen
+    for (let arg of arguments) {
+      if (arg === undefined || arg === null || arg === 0) {
+        return 0;
+      }
+    }
+
+    var safety_order_usage = 0.0;
+    var previous_safety_order = safety_order_size;
+    for (let i = 1; i <= max_safety_orders; i++) {
+      safety_order_usage += previous_safety_order;
+      previous_safety_order *= safety_order_scaling;
+    }
+    return this.normalize_numbers(base_order_size + safety_order_usage);
   },
 };
--- src/layout_manager.js
+++ src/layout_manager.js
@@ -1,18 +1,114 @@
+import { calculate_data } from "./calculate_data.js";
+
 export class LayoutManager {
   // the constructor
   constructor() {
     this.init_inputs();
+    this.init_outputs();
   }
 
   init_inputs() {
     // get the input elements
     const input_elements = document.querySelectorAll("input[type='number']");
     // add event listeners
+    // Go through each of the input fields and add an event listener`
+    // And also normalize the displayed number to be correct
     input_elements.forEach((element) => {
-      console.log("adding");
-      element.addEventListener("keyup", () => {
-        console.log("hello");
+      element.value =
+        element.id !== "maxsafetyordersinput" &&
+        element.id !== "numberofbotsinput"
+          ? calculate_data.normalize_numbers(Number(element.value))
+          : calculate_data.normalize_numbers_int(Number(element.value));
+      element.addEventListener("keyup", (element) => {
+        // Change the input to always be the correct number of decimals
+        // everything but Number of total safety orders and number of bots should be to two decimal places
+        // We'll wait 200ms and see if the input matches what was there
+        // If it's the same, make sure the format is correct
+        // If not, don't do anything
+        setTimeout(
+          (old_value, element_id) => {
+            if (old_value === document.getElementById(element_id).value) {
+              if (
+                element.target.id !== "maxsafetyordersinput" &&
+                element.target.id !== "numberofbotsinput"
+              ) {
+                element.target.value = calculate_data.normalize_numbers(
+                  Number(element.target.value)
+                );
+              } else {
+                element.target.value = calculate_data.normalize_numbers_int(
+                  Number(element.target.value)
+                );
+              }
+
+              // This is a bit of a weak stick approach
+              // Every key stroke on an input element is going to cause all values to be recalculated
+              // Even if the value isn't impacted by what is being updated
+              // Maybe should consider figuring out what element is being updated and only recalc
+              // relevant fields, but that feels difficult to track
+              this.set_total_cash();
+              this.set_amount_per_bot();
+              this.set_extra_cash();
+            }
+          },
+          200,
+          element.target.value,
+          element.target.id
+        );
       });
     });
+  }
+
+  init_outputs() {
+    this.set_total_cash();
+    this.set_amount_per_bot();
+    this.set_extra_cash();
+  }
+
+  set_total_cash() {
+    document.getElementById("totalcashoutput").innerHTML =
+      "$" + this.get_total_cash();
+  }
+
+  set_amount_per_bot() {
+    document.getElementById("cashusedperbotoutput").innerHTML =
+      "$" + this.get_amount_per_bot();
+  }
+
+  get_total_cash() {
+    return calculate_data.total_cash(
+      Number(document.getElementById("totalfreecashinput").value),
+      Number(document.getElementById("totalcashinbotsinput").value)
+    );
+  }
+
+  get_total_bots() {
+    return Number(document.getElementById("numberofbotsinput").value);
+  }
+
+  get_amount_per_bot() {
+    return calculate_data.amount_per_bot(
+      Number(document.getElementById("baseordersizeinput").value),
+      Number(document.getElementById("safetyordersizeinput").value),
+      Number(document.getElementById("safetyordersizescalinginput").value),
+      Number(document.getElementById("maxsafetyordersinput").value)
+    );
+  }
+
+  set_extra_cash() {
+    const extra = calculate_data.normalize_numbers(
+      this.get_total_cash() - this.get_amount_per_bot() * this.get_total_bots()
+    );
+
+    let extra_element = document.getElementById("extracashoutput");
+    extra_element.innerHTML = "$" + extra;
+
+    if (extra <= 0) {
+      extra_element.classList.add("red");
+      extra_element.classList.remove("green");
+    } else {
+      extra_element.classList.remove("red");
+      extra_element.classList.add("green");
+    }
   }
 }
