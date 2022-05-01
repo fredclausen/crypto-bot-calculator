bab651dddf4db390523bd63eb6f813e00ea854ef
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-05-01 13:34:41 -0600

    bug fix closing

--- src/index.js
+++ src/index.js
@@ -60,7 +60,6 @@ app.whenReady().then(() => {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-    save_bounds();
   });
 
   mainWindow.on("moved", () => {
