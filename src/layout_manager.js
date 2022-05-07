0622c936dd4cb51b30818c156c6d13c030bb3316
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-05-07 11:45:43 -0600

    Convert event listeners to jQuery

--- src/layout_manager.js
+++ src/layout_manager.js
@@ -25,64 +25,57 @@ export class LayoutManager {
 
   init_inputs() {
     // get the input elements
-    const input_elements = document.querySelectorAll("input[type='text']");
-    // add event listeners
-    // Go through each of the input fields and add an event listener`
-    // And also normalize the displayed number to be correct
-    input_elements.forEach((element) => {
-      element.value =
-        element.id !== element_ids.max_safety_orders &&
-        element.id !== element_ids.number_of_bots
-          ? calculate_data.normalize_numbers(Number(element.value))
-          : calculate_data.normalize_numbers_int(Number(element.value));
+    $("input[type='text']").each((_, element) => {
+      const ID = "#" + element.id;
+      const ID_NO_SELECTION = element.id;
 
-      // Event listener to listen for a double click and select all text
-      element.addEventListener("dblclick", (element) => {
-        element.target.focus();
-        element.target.select();
+      // add event listeners
+      // Go through each of the input fields and add an event listener`
+      // And also normalize the displayed number to be correct
+      $(ID).val(
+        ID_NO_SELECTION !== element_ids.max_safety_orders &&
+          ID_NO_SELECTION !== element_ids.min_safety_ordersnumber_of_bots
+          ? calculate_data.normalize_numbers(Number($(ID).val()))
+          : calculate_data.normalize_numbers_int(Number($(ID).val()))
+      );
+
+      // // Event listener to listen for a double click and select all text
+      $(ID).on("dblclick", () => {
+        $(ID).select();
+        $(ID).focus();
       });
 
-      // When the user de-focuses the input validate it fully
-      // Since the validator as they're typing ignores some stuff
-      element.addEventListener("focusout", (element) => {
+      // // When the user de-focuses the input validate it fully
+      // // Since the validator as they're typing ignores some stuff
+      $(ID).on("focusout", () => {
         if (
-          element.target.id !== element_ids.max_safety_orders &&
-          element.target.id !== element_ids.number_of_bots
+          ID_NO_SELECTION !== element_ids.max_safety_orders &&
+          ID_NO_SELECTION !== element_ids.number_of_bots
         ) {
-          element.target.value = calculate_data.normalize_numbers(
-            Number(element.target.value)
-          );
+          $(ID).val(calculate_data.normalize_numbers(Number($(ID).val())));
         } else {
-          element.target.value = calculate_data.normalize_numbers_int(
-            Number(element.target.value)
-          );
+          $(ID).val(calculate_data.normalize_numbers_int(Number($(ID).val())));
         }
 
         if (
-          element.target.id === element_ids.total_free_cash ||
-          element.target.id === element_ids.total_cash_in_bots
+          ID_NO_SELECTION === element_ids.total_free_cash ||
+          ID_NO_SELECTION === element_ids.total_cash_in_bots
         ) {
-          this.cash.update_unknown_value(
-            element.target.id,
-            element.target.value
-          );
+          this.cash.update_unknown_value(ID_NO_SELECTION, $(ID).val());
         } else {
-          this.bot_manager.update_unknown_value(
-            element.target.id,
-            element.target.value
-          );
+          this.bot_manager.update_unknown_value(ID_NO_SELECTION, $(ID).val());
         }
         this.init_or_update_outputs();
       });
 
-      element.addEventListener("keyup", (element) => {
+      $(ID).on("keyup", () => {
         // Get cursor positon
         // We need both the start/end so that the input box doesn't magically
         // Select shit we didn't want to select
 
-        let cursor_start_position = element.target.selectionStart;
-        let cursor_end_position = element.target.selectionEnd;
-
+        let cursor_start_position = $(ID).prop("selectionStart");
+        let cursor_end_position = $(ID).prop("selectionEnd");
+        let element_value = $(ID).val();
         // validate input is an actual number
         // We'll loop through the string input to see what the user has entered
         // This is a less elegant approach than simple find/replace all non-numbers
@@ -90,15 +83,15 @@ export class LayoutManager {
         // And I can't be bothered to write a regex to replace them
         // I'll just do this
 
-        if (!calculate_data.is_number(element.target.value)) {
-          for (let c of element.target.value) {
+        if (!calculate_data.is_number(element_value)) {
+          for (let c of element_value) {
             // We have to check for the decimal separator as well as if the character is a number
             // Otherwise we always drop the decimal which is annoying
             // TODO: Localize this. Lots of non-American countries use a comma as a decimal separator
             // This is partially localized. Is there other possibilities?
 
             if (!calculate_data.is_number(c) && !c === this.separator) {
-              element.target.value = element.target.value.replace(c, "");
+              $(ID).val(element_value.replace(c, ""));
               // SUPER important to move the cursor position back one so
               // The user input is at the same spot it was before
               cursor_start_position--;
@@ -112,10 +105,10 @@ export class LayoutManager {
         }
         // Change the input to always be the correct number of decimals
         // everything but Number of total safety orders and number of bots should be to two decimal places
-
+        element_value = $(ID).val();
         if (
-          element.target.id !== element_ids.max_safety_orders &&
-          element.target.id !== element_ids.number_of_bots
+          ID_NO_SELECTION !== element_ids.max_safety_orders &&
+          ID_NO_SELECTION !== element_ids.min_safety_ordersnumber_of_bots
         ) {
           // If the number DOES NOT include more than 2 decimal places don't change
           // The Value. If it does, change it to the correct number of decimal places
@@ -125,18 +118,18 @@ export class LayoutManager {
           // And let it round.
 
           if (
-            element.target.value.includes(".") &&
-            element.target.value.split(".")[1].length > 2
+            element_value.includes(".") &&
+            element_value.split(".")[1].length > 2
           ) {
-            element.target.value = calculate_data.normalize_numbers(
-              Number(
-                element.target.value.slice(0, element.target.value.length - 1)
+            $(ID).val(
+              calculate_data.normalize_numbers(
+                Number(element_value.slice(0, element_value.length - 1))
               )
             );
           }
         } else {
-          element.target.value = calculate_data.normalize_numbers_int(
-            Number(element.target.value)
+          $(ID).val(
+            calculate_data.normalize_numbers_int(Number(element_value))
           );
         }
 
@@ -144,8 +137,8 @@ export class LayoutManager {
         // Start and End selection points are set so that it doesn't
         // highlight text that wasn't highlighted before
 
-        element.target.selectionStart = cursor_start_position;
-        element.target.selectionEnd = cursor_end_position;
+        $(ID).prop("selectionStart", cursor_start_position);
+        $(ID).prop("selectionEnd", cursor_end_position);
 
         // This is a bit of a weak stick approach
         // Every key stroke on an input element is going to cause all values to be recalculated
